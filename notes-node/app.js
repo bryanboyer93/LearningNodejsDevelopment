4664442a96ebfe3abd0e4a01b4b970dbe352d2ba
bryanboyer93 <bryanriik@gmail.com>
2018-09-10 21:56:16 -0400

    Lodash .isString() and uniq() Examples

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,12 +2,23 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require ('./notes.js');
 
 console.log('Result:', notes.add( 9,-2));
 
-var user = os.userInfo();
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
-    if (err) throw err;
-    // console.log('The "data to append" was appended to the file!');
-});
+// lodash isString()
+// console.log(_.isString(true));
+// console.log(_.isString('Gary'));
+
+// lodash uniqu()
+var testArray = ['Gary',1,'Gary',1,2,3,4];
+console.log(`Before _.uniq(): ${testArray}`);
+var filteredArray = _.uniq(testArray);
+console.log(`After _.uniq(): ${filteredArray}`);
+
+// var user = os.userInfo();
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, (err) => {
+//     if (err) throw err;
+//     // console.log('The "data to append" was appended to the file!');
+// });
