3097ab257e3705eac8c6847cc487427cd5356487
bryanboyer93 <42813435+bryanboyer93@users.noreply.github.com>
2018-09-12 16:46:05 -0400

    update

--- playground/json.js
+++ playground/json.js
@@ -1,11 +1,22 @@
+const fs = require('fs');
+
 var obj = {
     name: 'Andrew'
-}
+};
+
+var originalNote = {
+    title: 'Some title',
+    body: 'Some body'
+};
 
 var stringObj = JSON.stringify(obj);
-
 console.log(typeof stringObj);
 console.log(stringObj);
 
-var personString = '{"name" : "Andrew", :"age": 25}';
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var personString = '{"name" : "Andrew", "age": 25}';
 var person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
