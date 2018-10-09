2039ed4576f51ecb2b3de49ff74d13ebb479ae33
bryanboyer93 <42813435+bryanboyer93@users.noreply.github.com>
2018-10-09 13:28:38 -0400

    finishes part 1 of async

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,18 @@
 const request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyB9c2sRN0T2AVtXLCCoCf4tjVUsfIE7CVE'
-    //json: true
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyB9c2sRN0T2AVtXLCCoCf4tjVUsfIE7CVE',
+    json: true
+    // you can access the body from response.body as well
   }, (error, response, body) => {
-    //   console.log(body);
-        console.log(JSON.stringify(body, undefined, 2));
-
+      //   console.log(body);
+      // console.log(JSON.stringify(body, undefined, 2));
       // To print json object in a pretty way
       // JSON.stringify(body, filter, spacesPerIndentationk)
-
-      // you can access the body from response.body as well
-
+      //console.log(body.results);
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
       
   });
 
