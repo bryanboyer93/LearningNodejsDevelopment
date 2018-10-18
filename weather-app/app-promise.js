d513a1a7e455e1fbe205e587545655d9de6e8432
bryanboyer93 <42813435+bryanboyer93@users.noreply.github.com>
2018-10-18 09:14:45 -0400

    Update weather-app/app-promise.js

---
+++ weather-app/app-promise.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Address to fetch weather for'
+      // string: true //parses argument as a string
+    }
+  })
+  .help()
+  .alias('help', 'h') // two arguments, argument, alias
+  .argv; // takes config, runs it through args, then restores the result in the argv variable
+
+  
